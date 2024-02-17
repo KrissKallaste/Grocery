0712fbc1d8ab8cc6238833e9107db405c978e0d5
Krisskallaste <Kallastekriss1998@gmail.com>
2024-02-17 09:56:08 +0200

    Second commit

--- src/main/java/Grocery/Main.java
+++ src/main/java/Grocery/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("This is grocery store");
+
         boolean quit = false;
         int choice = 0;
         printInstructions();
